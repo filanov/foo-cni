c1635dc695e6d0591bff5b339dc9c5d8826db4ea
Michael Filanov <mfilanov@nvidia.com>
2024-12-02 13:16:58 +0200

    log data to syslog

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"log/syslog"
 	"os"
 )
 
 func main() {
+
+	// Log to syslog
+	logWriter, err := syslog.New(syslog.LOG_SYSLOG, "My Awesome App")
+	if err != nil {
+		log.Fatalln("Unable to set logfile:", err.Error())
+	}
+	// set the log output
+	log.SetOutput(logWriter)
+
 	// Parse command (ADD/DEL)
 	cmd := os.Getenv("CNI_COMMAND")
 	args := os.Getenv("CNI_ARGS")
 
-	fmt.Println("CNI ARGS", args)
+	log.Println("CNI ARGS", args)
 
 	switch cmd {
 	case "ADD":
-		fmt.Println("Setting up network...")
+		log.Println("Setting up network...")
 		// Add your network setup logic here
 		// fmt.Fprintf(os.Stdout, `{"cniVersion": "%s", "interfaces": [{"name": "eth0"}]}`, config.CniVersion)
 	case "DEL":
-		fmt.Println("Cleaning up network...")
+		log.Println("Cleaning up network...")
 		// Add your network cleanup logic here
 	default:
-		fmt.Fprintf(os.Stderr, "Unsupported CNI_COMMAND: %s\n", cmd)
+		log.Printf("Unsupported CNI_COMMAND: %s\n", cmd)
 		os.Exit(1)
 	}
 }
