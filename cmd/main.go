56ffe2867b70bb43578c4d4504c41d52c6be2e21
Michael Filanov <mfilanov@nvidia.com>
2024-12-04 16:04:15 +0200

    add dummy reply to the cni

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/cni/pkg/types"
+	current "github.com/containernetworking/cni/pkg/types/100"
 	cni "github.com/containernetworking/cni/pkg/version"
 )
+
+var cniVersion = "0.3.1"
 
 func main() {
 	logFile, err := os.OpenFile("/var/log/foo-cni.log", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
@@ -24,39 +27,38 @@ func main() {
 		Check:  checkCmd,
 		GC:     gcCmd,
 		Status: statusCmd,
-	}, cni.All, "v1")
+	}, cni.All, cniVersion)
 }
 
 func addCmd(args *skel.CmdArgs) error {
 	log.Printf("Add iface: %s args: %s\n", args.IfName, args.Args)
 	log.Printf("Add config: %s", string(args.StdinData))
 
-	fmt.Println("{}")
-	return nil
+	result := &current.Result{CNIVersion: current.ImplementedSpecVersion}
+	return types.PrintResult(result, cniVersion)
 }
 
 func delCmd(args *skel.CmdArgs) error {
 	log.Printf("del iface: %s args: %s\n", args.IfName, args.Args)
 	log.Printf("del config: %s", string(args.StdinData))
-	fmt.Println("{}")
-	return nil
+	result := &current.Result{CNIVersion: current.ImplementedSpecVersion}
+	return types.PrintResult(result, cniVersion)
 }
 
 func checkCmd(args *skel.CmdArgs) error {
 	log.Printf("Check %+v", args)
-	fmt.Println("{}")
-	return nil
+	result := &current.Result{CNIVersion: current.ImplementedSpecVersion}
+	return types.PrintResult(result, cniVersion)
 }
 
 func gcCmd(args *skel.CmdArgs) error {
 	log.Printf("GC %+v", args)
-	fmt.Println("{}")
-	return nil
+	result := &current.Result{CNIVersion: current.ImplementedSpecVersion}
+	return types.PrintResult(result, cniVersion)
 }
 
 func statusCmd(args *skel.CmdArgs) error {
 	log.Printf("Status %+v", args)
-	fmt.Println("{}")
-	fmt.Println("{}")
-	return nil
+	result := &current.Result{CNIVersion: current.ImplementedSpecVersion}
+	return types.PrintResult(result, cniVersion)
 }
